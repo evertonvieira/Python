a4c880298d29eddc871cb2a9d977df45b9881cfb
LAbime <tste@teste.com>
2015-09-28 18:36:48 -0300

    Alterando mensagem para o usuário

--- somatorio.py
+++ somatorio.py
@@ -1,6 +1,6 @@
 import math
 x = input ("Digite o valor de X: ")
-y = input ("Digite o valor de X: ")
+y = input ("Digite o valor de Y: ")
 e = 0
 for i in range(1, 51):
 	e = e + (x + (math.sqrt(y + 2)))/3 + (i**2)-(y**5)
