560bb97935d06b1b1aebad957fc2608d3a376727
Everton Vieira <evertonvieira1994@gmail.com>
2015-10-15 08:53:15 -0300

    Adicionando exercício do número perfeito

--- Lista-Prova-Pratica/numero-perfeito.py
+++ Lista-Prova-Pratica/numero-perfeito.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-# 7. Imprima os números perfeitos entre 0 e 1000. Um número é dito perfeito quando a soma dos seus divisores (tirando ele próprio) é igual a ele.
+#Imprima os números perfeitos entre 0 e 1000. Um número é dito perfeito quando a
+#soma dos seus divisores (tirando ele próprio) é igual a ele.
 #Exemplo: 6 é perfeito pois, 1 + 2 + 3 = 6
+
+#n = 1000
 soma = 0
-somaPerfe = 0
-n = input ("número: ")
-for i in range(0, 1001):
-	if i % 2 == 0:
-		soma += i
-		print soma
-		if (soma == n):
-			somaPerfe += soma
-
-print "soma:", somaPerfe
-
+for i in range(1, 1000):
+	n = i
+	soma = 0
+	for j in range(1, n):
+		if n % j == 0:
+			soma += j
+	if soma == n:
+		print n,
